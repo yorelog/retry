401efea64d3dd1eb0ccabc4fd987a42d5c7f8590
yorelog <yorelog@gmail.com>
2023-08-25 10:00:50 +0800

    add mac&& linux support

--- src/main.rs
+++ src/main.rs
@@ -3,72 +3,61 @@ use std::env;
 use std::thread;
 use std::time::Duration;
 
-
-
-fn main() {
+fn main(){
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() < 2 {
-        println!("Usage: retry <command>");
-        return;
+        println!("Retry command execution until it succeeds or max retry count is exceeded.\n");
+        println!("Usage: retry <command>\n\n");
+        println!("Example: retry git pull\n");
+        std::process::exit(1);
     }
 
-    let cmd= &args[1..].join(" ");
+    let cmd = &args[1..].join(" ");
     let max_retry_count = 30;
     let mut retry_count = 0;
     let mut last_error_msg = String::new();
 
+    let exec_cmd = if cfg!(windows) {"cmd"} else {"sh"};
+    let exec_arg = if cfg!(windows) {"/C"} else {"-c"};
     loop {
         let current_dir = env::current_dir().unwrap();
         println!("Current dir: {:?}",current_dir);
 
-        let output = if cfg!(windows){
-            Command::new("cmd")
-            .arg("/C")
+        let output = Command::new(exec_cmd)
+            .arg(exec_arg)
             .arg(cmd)
             .current_dir(&current_dir)
             .stdout(Stdio::piped())
             .stderr(Stdio::piped())
-            .output()
-
-        }else {
-            Command::new("sh")
-            .arg("-c")
-            .arg(cmd)
-            .current_dir(&current_dir)
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .output()
-        };
-
-    
+            .output();
         match output {
-            Ok(output) => {
+            Ok(output) =>{
                 if output.status.success() {
                     let stdout = String::from_utf8_lossy(&output.stdout).to_string();
-                    println!("Command executed successfully!\nOutput:\n{}",stdout);
+                    println!("Command executed sucessfully!\nOutput:\n{}",stdout);
                     break;
-
-                }else {
+                } else {
                     last_error_msg = String::from_utf8_lossy(&output.stderr).to_string();
                     println!("Error occurred during command execution:\n{}",last_error_msg);
+
                 }
             }
-            Err(e) =>{
+            Err(e) => {
                 last_error_msg = e.to_string();
                 println!("Error occurred during command execution:\n{}",last_error_msg);
             }
         }
         if retry_count >= max_retry_count {
-            println!("Max retry count exceeded...");
+            println!("Max retry count execeeded...");
             break;
         }
-
-        retry_count += 1;
-        println!("Retry count: {} ", retry_count);
+        retry_count +=1;
+        println!("Retry {} times",retry_count);
         thread::sleep(Duration::from_secs(1));
     }
     if last_error_msg.is_empty() {
         println!("Command executed successfully!");
     }
+
 }
